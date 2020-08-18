7c8c4340404f82519a3bfe7e02ad660ba321db97
leeja <Leejaeyun95@naver.com>
2020-08-19 00:35:38 +0900

    파이썬 300- 문자열 시작

---
+++ python 300/string python/String.py
@@ -0,0 +1,15 @@
+# 021 문자열 인덱싱
+letters = 'python'
+print(letters[0], letters[2])   # 한 글자씩 가져오는 것을 '인덱싱'이라고 한다
+
+
+# 022 문자열 슬라이싱
+license_plate = "24가 2210"
+print(license_plate[-4])  # 일정 구간을 슬라이싱(slicing)해서 가져온다
+
+string = '{ytn'+에 다녀감, 
+
+
+# 023 문자열 인덱싱
+wow = "홀짝홀짝홀짝"
+print(wow[::2])
