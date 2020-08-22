bc5491db6c8b0cfbc68e8330a16659f51cf2d732
leeja <Leejaeyun95@naver.com>
2020-08-22 23:14:04 +0900

    파이썬  for문 131~140 완료

---
+++ python 300/loop python/Loop131~140.py
@@ -0,0 +1,39 @@
+# 131
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+    print(변수)
+
+# 132
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+  print("#####")
+
+# 133 pass
+
+# 134 pass
+
+# 135
+print("A".lower())
+print("B".lower())
+print("C".lower())
+
+# 136
+for i in (10,20,30):
+    print(i)
+
+# 137
+for i in (10,20,30):
+    print(i)
+
+# 138
+for i in (10,20,30):
+    print(i)
+    print("--------")
+# 139
+print("+++++")
+for i in (10,20,30):
+    print(i)
+
+# 140
+for i in range(4):
+    print("---------")
