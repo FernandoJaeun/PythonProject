1fd47e56ac3e054a5c8fb7f9460aba1744ca0220
leeja <Leejaeyun95@naver.com>
2020-08-22 17:59:29 +0900

    파이썬 분기문 완료

--- python 300/divergence python/Divergence121~130.py
+++ python 300/divergence python/Divergence121~130.py
@@ -17,7 +17,7 @@
 #     print("D")
 # else:
 #     print("E")
-    
+
 
 # # 123
 # moneys = {'달러' : 1167, '엔' : 1.096, '유로' : 1268 , '위안' : 171}
@@ -40,21 +40,66 @@
 # print(max(num_list))
 
 
-# 125
-num_list = {'011' :'SKT', 
-            '016' :'KT' , 
-            '019' :'LGU', 
-            '010' : '알수없음' }
-user = input("number = ")
-user_phone = user.split("-")[0]
-if user_phone in list(num_list.keys()):
-    print(f"당신은 {num_list[user_phone]} 사용자 입니다")
-else :
-    print("니 누꼬?")
+# # 125
+# num_list = {'011': 'SKT',
+#             '016': 'KT',
+#             '019': 'LGU',
+#             '010': '알수없음'}
+# user = input("number = ")
+# user_phone = user.split("-")[0]
+# if user_phone in list(num_list.keys()):
+#     print(f"당신은 {num_list[user_phone]} 사용자 입니다")
+# else:
+#     print("니 누꼬?")
 
- 
-# 126
-# 127
-# 128
-# 129
+
+# # 126
+# gus = ['강북구', '강북구', '강북구',
+#        '도봉구', '도봉구', '도봉구',
+#        '노원구', '노원구', '노원구', '노원구']
+# nums = [0, 1, 2,
+#         3, 4, 5,
+#         6, 7, 8, 9]
+# zipcode = dict(zip(nums, gus))
+
+# user = list(input("우편번호를 입력 : "))
+# user_zip = int(user[2])
+
+# if user_zip in nums:
+#     print(zipcode[user_zip])
+
+
+# # 127
+# RRnum = input("당신 주민 번호 입력 : ")
+# gender = RRnum.split("-")[1]
+# gender = gender[0]
+# print(gender)
+# if gender == ('1' or '3'):
+#     print("성별은 남성")
+# else:
+#     print("성별은 여성")
+
+# # 128
+# RRnum = input("당신 주민 번호 입력 : ")
+# region = RRnum.split("-")[1][1:3]
+# seoul = ['00','01','02','03','04','05','06','07','08']
+# if region in seoul :
+#     print("출생지는 서울입니다")
+# else:
+#     print("서울 사람이 아닙니다")
+
+
+# # 129 sun of beach
+# RRnum = input("당신 주민 번호 입력 : ")
+# RRnum = RRnum.split("-")
+# validation = [[2, 3, 4, 5, 6, 7], [8, 9, 2, 3, 4, 5]]
+
 # 130
+
+import requests
+url = "https://api.bithumb.com/public/ticker/"
+btc =  requests.get(url).json()['data']
+opening_price = btc['opening_price']
+closing_price = btc['closing_price']
+min_price = btc['min_price']
+max_price= btc['max_price']
