9f996981a10cae5487fe5ee0248eedcc5bbc9248
leeja <Leejaeyun95@naver.com>
2020-08-23 18:07:26 +0900

    파이썬 함수 211~220 완료

---
+++ python 300/function python/Function211~220.py
@@ -0,0 +1,59 @@
+# # 211
+# def 함수(문자열) :
+#     print(문자열)
+
+# 함수("안녕")
+# 함수("Hi")
+
+# # 212
+# def 함수(a, b) :
+#     print(a + b)
+
+# 함수(3, 4)
+# 함수(7, 8)
+
+# # 213
+# def 함수(문자열) :
+#     print(문자열)
+
+# # 214
+# def 함수(a, b) :
+#     print(a + b)
+
+# 함수("안녕", 3)
+
+# 215
+def print_with_smile(in_str):
+    print(in_str, ":D")
+
+# 216
+print_with_smile("안녕하세요")
+
+# 217
+def print_upper_price(presentPrice):
+    print(presentPrice * 1.3)
+
+# 218
+def print_sum(a, b):
+    print(a+b)
+
+# 219
+def  print_arithmetic_operation(a, b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+
+# 220
+def print_max(a,b,c):
+    max_val = 0
+    if a > max_val:
+        max_val = a
+    if b > max_val:
+        max_val = b
+    if c > max_val:
+        max_val = c
+    print(max_val)
+    
+print_max(8,8,7)
+
