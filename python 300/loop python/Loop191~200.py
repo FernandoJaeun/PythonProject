a22c7a2f17a08c07f57b30e41258c3b588eb6f62
leeja <Leejaeyun95@naver.com>
2020-08-23 17:47:36 +0900

    파이썬 반복문 완료

--- python 300/loop python/Loop191~200.py
+++ python 300/loop python/Loop191~200.py
@@ -1,10 +1,116 @@
-# 191
-# 192
-# 193
-# 194
-# 195
-# 196
-# 197
-# 198
-# 199
+# # 191
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# tax = 0.00014
+# for row in data:
+#     for col in row:
+#         print(col + (tax * col))
+#     #   print(col * 1.00014)
+
+# # 192
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# tax = 0.00014
+# for row in data:
+#     for col in row:
+#         print(col * 1.00014)
+#     print("----------")
+
+
+# # 193
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# tax = 0.00014
+# result = []
+# for row in data:
+#     for col in row:
+#         result.append((col * 1.00014))
+# print(result)
+
+# # 194
+# data = [
+#     [2000,  3050,  2050,  1980],
+#     [7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# tax = 0.00014
+# result = []
+# for row in data:
+#     sub = []
+#     for col in row:
+#         sub.append((col * 1.00014))
+#     result.append(sub)
+# print(result)
+
+
+# # 195
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for i in ohlc[1:]:
+#     for j in i[-1:]:
+#         print(j)
+# for i in ohlc[1:]:
+#     print(i[-1])
+
+
+# # 196
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for row in ohlc[1:]:
+#     if row[-1] > 150:
+#         print(row[-1])
+
+# # 197
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for row in ohlc[1:]:
+#     if row[-1] >= row[0]:
+#         print(row[-1])
+
+
+# # 198
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# volatility = []
+# for row in ohlc[1:]:
+#     volatility.append((row[1] - row[2]))
+# print(volatility)
+
+
+# # 199
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# volatility = []
+# for row in ohlc[1:]:
+#     if row[-1] > row[0]:
+#          print(row[1] - row[2])
+
+
 # 200
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+result = 0
+for row in ohlc[1:]:
+    result += (row[0] - row[-1])
+print(result)
