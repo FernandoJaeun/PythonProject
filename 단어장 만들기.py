6d7a8ef3908ea91410c8767a4b6e94971c5129fa
leeja <Leejaeyun95@naver.com>
2020-09-01 21:19:34 +0900

    단어장 만들기 풀이

---
+++ 단어장 만들기.py
@@ -0,0 +1,15 @@
+with open(file="C:/Users/leeja/WorkSpace/Python/Python Project/sub/vocaulary.txt",
+          mode='a',
+          encoding='utf-8') as file:
+    while True:
+        context = input("영어 단어를 입력하세요: ")
+        if context == "q":
+            break
+        file.write(context + "\n")
+
+        context = input("한국어 뜻을 입력하세요: ")
+        if context == "q":
+            break
+        file.write(context + "\n\n")
+
+        print("")
